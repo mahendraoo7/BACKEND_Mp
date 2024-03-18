dfcbfd86a5d262defef0f84dbe8c64e8ca1289ac
Mahendra purohit <Mahendrapurohitmewada@gmail.com>
2024-03-18 11:15:30 +0530

    18-03-2024

--- Express/Routes/cart.routes.js
+++ Express/Routes/cart.routes.js
@@ -4,13 +4,17 @@ const { verifyToken} = require('../helpers/verifyToken');
 const {
     addToCart,
     getAllCart, 
-    getCart
+    getCart,
+    updateCart,
+    deleteCart 
 } = require('../controller/cart.controller');
 
 
 cartRoutes.post('/add-cart', verifyToken,addToCart);
-cartRoutes.get('/get-carts', verifyToken,getAllCart);
 cartRoutes.get('/get-cart', verifyToken, getCart);
+cartRoutes.get('/get-carts', verifyToken,getAllCart);
+cartRoutes.put('/update-cart', verifyToken, updateCart);
+cartRoutes.delete('/delete-cart', verifyToken, deleteCart);
 
 
 module.exports = cartRoutes;
