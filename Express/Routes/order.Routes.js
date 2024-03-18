75725e84a70af48da5833093aa7b19ce76b64b71
Mahendra purohit <Mahendrapurohitmewada@gmail.com>
2024-03-18 11:09:59 +0530

    18-03-2024

--- Express/Routes/order.Routes.js
+++ Express/Routes/order.Routes.js
@@ -3,10 +3,12 @@ const orderRoutes = express.Router()
 const { verifyToken} = require('../helpers/verifyToken');
 const {
     newOrder,
-    getAllOrders 
+    getAllOrders,
+    getOrder 
 }  =require('../controller/order.controller');
 
 orderRoutes.post('/add-order',verifyToken , newOrder);
 orderRoutes.get('/get-all-order',verifyToken, getAllOrders);
+orderRoutes.get('/get-order',verifyToken, getOrder);
 
 module.exports = orderRoutes;
--- Express/controller/order.controller.js
+++ Express/controller/order.controller.js
@@ -4,23 +4,23 @@ const Cart = require('../model/cart.model');
 exports.newOrder = async (req,res) => {
     try {
         let cartItems = await Cart.find({user : req.user._id, isDelete :false }).populate('cartItem');
-        res.send(cartItems);
+        // res.send(cartItems)
         let orderItems =  cartItems.map(item => ({
              product : item.cartItem._id,
-             quantity : item.cartItem._id,
-             price : item.cartItem.price
+             quantity : item.quantity,
+             price : item.cartItem.price    
         }));
-        console.log(orderItems);
-        let totalPrice = orderItems.reduce((total,item) => total+(item.price * item.quantity),100); 
-        console.log(totalPrice);
-        // let newOrder = await Order.create ({
-        //     user : req.user._id,
-        //     items : orderItems,
-        //     totalAmount : totalPrice
-        // });
-        // newOrder.save();
-        // await Cart.updateMany({ user : req.user._id},{ $set : {isDelete : true} });
-        // res.status(200).json({Order : newOrder, message : 'Order Place Success'});
+        // console.log(orderItems);
+        let totalPrice = orderItems.reduce((total,item) => total + (item.price * item.quantity),100); 
+        // console.log(totalPrice);
+        let newOrder = await Order.create ({
+            user : req.user._id,
+            items : orderItems,
+            totalAmount : totalPrice
+        });
+        newOrder.save();
+        await Cart.updateMany({ user : req.user._id},{ $set : {isDelete : true} });
+        res.status(201).json({Order : newOrder, message : 'Order Place Success'});
 
     }catch(error) {
         console.log(error);
@@ -30,10 +30,21 @@ exports.newOrder = async (req,res) => {
 
   exports.getAllOrders = async (req,res) => {
     try { 
-         let orders = await Order.find({user: req.user._id ,isDelete : false});
+         let orders = await Order.find({ order : Order._id},{isDelete : false});
          res.status(200).json(orders);
     } catch (error) {
         console.log(error);
         res.status(500).json({ message : 'Interal Server Error'});
     } 
-  }
+  };
+
+
+  exports.getOrder = async (req,res) => {
+    try { 
+         let order = await Order.find({ orderId : Order._id },{isDelete : false});
+         res.status(200).json(order);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message : 'Interal Server Error'});
+    }
+};
