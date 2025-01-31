21fe897301f297e58e25598ddce9bbd4e2163886
mahendraoo7 <mahendrapurohit9889@gmail.com>
2025-01-31 10:51:05 +0530

    code

--- Crude_node/Express_Crud/Server.js
+++ Crude_node/Express_Crud/Server.js
@@ -3,9 +3,9 @@ const app = express();
 
 app.get('/', (req, res) => {
 
-    res.send("Hello world");
-})
+    res.send("Hello world");  
+})  
 
 app.listen(4000, () => {
     console.log(`Server start at http://localhost:4000`);
-})
+})   
